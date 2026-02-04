2928e34e06429b3b59f4c477864c53f4769fe1e5
Zeynep Yildirim <zeyildir@k1m08s05.42istanbul.com.tr>
2026-02-04 20:32:05 +0300

    first commit

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hakalkan <hakalkan@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: zeyildir <zeyildir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 04:15:59 by hakalkan          #+#    #+#             */
-/*   Updated: 2026/02/04 16:33:28 by hakalkan         ###   ########.fr       */
+/*   Updated: 2026/02/04 17:56:08 by zeyildir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,41 @@
 # include <sys/wait.h>
 # include "libft/libft.h"
 
-typedef enum e_token_type
+
+typedef enum e_token_type //tokenizerın etiketleri için
 {
 	TWORD,
-	TPİPE,
-	T_REDİR_IN,		// <
-	T_REDİR_OUT,    // >
+	TPIPE,
+	T_REDIR_IN,		// <
+	T_REDIR_OUT,    // >
 	T_REDIR_APPEND, // >>
 	T_HEREDOC, // <<
 }t_token_type;
 
 
-typedef struct s_token
+typedef struct s_token //tokenizerın çıktısı için, parser bu listeyi okur. parserın bir sıraya ihtiyacı var ondan enum yeterli değil
 {
 	char 	*value;
-	t_token_type	type;
+	t_token_type	type; //bu token ne
 	struct s_token	*next;
 	
-}	t_token;
+}	t_token; // quot bilgisi yok
 
-typedef struct s_redir
+typedef struct s_redir //parserın redirectionlarını için
 {
 	t_token_type	type;
-	char			*file;
+	char			*file; // out.txt
 	struct s_redir	*next;
 }	t_redir;
 
-typedef struct s_command
+typedef struct s_command //parserın  ana çıktısı (çalıştırılacak olan komut)
 {
-	char	**argv;
-	t_redir			*redirs;
-	int				has_pipe;
-	struct s_command	*next;	
+	char	**argv; //execveye gidecek olan
+	t_redir			*redirs; //input output ayarları
+	struct s_command	*next;	 //pipeline için
 }t_command;
 
-typedef struct s_shell
+typedef struct s_shell //shell state. komutlar arası kaybolmaması gereken bilgileri tutar
 {
 	t_token		*tokens;
 	t_command	*commands;
