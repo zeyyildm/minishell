4cf905a1c0eebcfd576b1d46427b756e844558f4
zeyyildm <zeynepyildirimak7641@gmail.com>
2026-03-07 23:32:54 +0300

    heredoc write kismi yazilacak

--- exec_heredoc.c
+++ exec_heredoc.c
@@ -12,15 +12,60 @@
 
 #include "minishell.h"
 
-void exec_heredoc(t_command *cmd)
+//heredoc mini bir pipe gibi davraniyor
+//shell heredoc icin gecici bir pipe acar
+// fd[1] ucu = heredoc yazma ucu
+// fd[0] ucu = komut okuma ucu
+
+//diyelim ki cat << EOF
+
+void exec_heredoc(t_shell *shell, t_command *cmd)
 {
     t_command *tmp;
 
     tmp = cmd;
     while (tmp)
     {
-        if(tmp->redirs->type )
+        if(tmp->redirs && tmp->redirs->type == T_HEREDOC) //redir varsa ve bu redir de heredocsa
+        {
+            int     fd[2];
+            pid_t   pid;
+            if(pipe(fd) == -1)
+            {
+                perror("heredoc hatasiii");
+                return;
+            }
 
+
+
+            
+            //bu arada write ile heredoc satirlarini yazdirmamiz lazim
+
+
+
+
+
+
+            pid = fork();
+            if(pid < 0)
+            {
+                perror("heredoc child hatasii"); 
+                return;
+            }
+            if(pid == 0)
+            {
+                dup2(fd[0], STDIN_FILENO); //girisi heredoc pipeindan
+                close(fd[0]);
+                //tmp şu an zaten cati tutuyor cati çalıştırmamiz lazim
+                if(init_builtin_ex(shell, tmp) == -1)
+                    execute_basic(shell, tmp);
+                exit(1);
+            }
+    
+            close(fd[0]); //parent icin de fd kapanmali
+            waitpid(pid, NULL, 0);
+
+        }
         tmp = tmp->next;
     }
     
