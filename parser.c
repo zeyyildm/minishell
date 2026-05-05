8ad3498ffedb1e24c2469807403a3411c1514148
Hayrullah" Kalkan <hakalkan@k1m03s10.42istanbul.com.tr>
2026-05-05 17:08:42 +0300

    redirect is norm finish

--- parser.c
+++ parser.c
@@ -6,137 +6,136 @@
 /*   By: hakalkan <hakalkan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 16:32:06 by hakalkan          #+#    #+#             */
-/*   Updated: 2026/04/07 18:28:48 by hakalkan         ###   ########.fr       */
+/*   Updated: 2026/05/05 17:03:08 by hakalkan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_redir *add_redir(void)
+static	t_redir	*add_redir(void)
 {
-    t_redir *rdr;
+	t_redir	*rdr;
 
-    rdr = malloc(sizeof(t_redir));
-    if (!rdr)
-        return NULL;
-    rdr->file = NULL;
-    rdr->type = 0;
-    rdr->next = NULL;
-    return rdr;
+	rdr = malloc(sizeof(t_redir));
+	if (!rdr)
+		return (NULL);
+	rdr->file = NULL;
+	rdr->type = 0;
+	rdr->next = NULL;
+	return (rdr);
 }
 
-
-t_command *add_command(void)
+t_command	*add_command(void)
 {
-    t_command *cmd;
-    
-    cmd = malloc(sizeof(t_command));
-    if (!cmd)
-        return NULL;
-    cmd->argv = NULL;
-    cmd->next = NULL;
-    cmd->redirs = NULL;
-    cmd->i = 0;
-    cmd->heredoc_fd = -1;
+	t_command	*cmd;
 
-    return cmd;
+	cmd = malloc(sizeof(t_command));
+	if (!cmd)
+		return (NULL);
+	cmd->argv = NULL;
+	cmd->next = NULL;
+	cmd->redirs = NULL;
+	cmd->i = 0;
+	cmd->heredoc_fd = -1;
+	return (cmd);
 }
 
-int cmd_size(t_token *t)
+int	cmd_size(t_token *t)
 {
-    int i;
-    t_token *tmp;
+	int		i;
+	t_token	*tmp;
 
-    i = 0;
-    tmp = t;
-    while (tmp && tmp->type != TPIPE)
-    {
-        if (tmp->type == T_REDIR_IN || tmp->type == T_REDIR_OUT ||tmp->type == T_REDIR_APPEND || tmp->type == T_HEREDOC)
-            tmp = tmp->next;
-        else if(tmp->type == TWORD)
-            i++;
-        tmp = tmp->next;
-    }
-    return i;
+	i = 0;
+	tmp = t;
+	while (tmp && tmp->type != TPIPE)
+	{
+		if (tmp->type == T_REDIR_IN || tmp->type == T_REDIR_OUT
+			|| tmp->type == T_REDIR_APPEND || tmp->type == T_HEREDOC)
+			tmp = tmp->next;
+		else if (tmp->type == TWORD)
+			i++;
+		tmp = tmp->next;
+	}
+	return (i);
 }
 
-int word_save(t_command *cmd , t_token *t)
+int	word_save(t_command *cmd, t_token *t)
 {
-    t_token *tmp;
-    int count;
-    int i;
+	t_token	*tmp;
+	int		count;
+	int		i;
 
-    count = 0;
-    tmp = t;   
-    i = 0; 
-    while (tmp && tmp->type != TPIPE) //pipe gelene kadar tüm tokenları gez
-    {
-        if (tmp->next && (tmp->type == T_REDIR_IN || tmp->type == T_REDIR_OUT ||tmp->type == T_REDIR_APPEND || tmp->type == T_HEREDOC))
-            tmp = tmp->next; //redirleri atla argvye koyma
-
-        else if(tmp->type == TWORD)
-        {
-            cmd->argv[i++] = ft_strdup(tmp->value); // raw token sakla, expanded() işleyecek
-            count++;
-        }
-        tmp = tmp->next;
-    }
-    cmd->argv[i] = NULL;
-    cmd->i = i;
-    return count;
+	count = 0;
+	tmp = t;
+	i = 0;
+	while (tmp && tmp->type != TPIPE) //pipe gelene kadar tüm tokenları gez
+	{
+		if (tmp->next && (tmp->type == T_REDIR_IN || tmp->type == T_REDIR_OUT
+				||tmp->type == T_REDIR_APPEND || tmp->type == T_HEREDOC))
+			tmp = tmp->next; //redirleri atla argvye koyma
+		else if (tmp->type == TWORD)
+		{
+			cmd->argv[i++] = ft_strdup(tmp->value); // raw token sakla, expanded() işleyecek
+			count++;
+		}
+		tmp = tmp->next;
+	}
+	cmd->argv[i] = NULL;
+	cmd->i = i;
+	return (count);
 }
-void redir_add_back(t_command *cmd, t_redir *new)
-{
-    t_redir *tmp;
 
-    if (!cmd->redirs)
-    {
-        cmd->redirs = new;
-        return;
-    }
-    tmp = cmd->redirs;
-    while (tmp->next)
-        tmp = tmp->next;
-    tmp->next = new;
+void	redir_add_back(t_command *cmd, t_redir *new)
+{
+	t_redir	*tmp;
+
+	if (!cmd->redirs)
+	{
+		cmd->redirs = new;
+		return ;
+	}
+	tmp = cmd->redirs;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 }
 
 void redir_pars(t_command *cmd, t_token *t)
 {
-    t_redir *new;
+	t_redir	*new;
 
-    if (!t->next || !t->next->value)
-        return;
-    new = add_redir();
-    new->type = t->type;
-    new->file = ft_strdup(t->next->value);
-    redir_add_back(cmd, new);
+	if (!t->next || !t->next->value)
+		return ;
+	new = add_redir();
+	new->type = t->type;
+	new->file = ft_strdup(t->next->value);
+	redir_add_back(cmd, new);
 }
 
 t_command *parser(t_token *t, t_command *cmd)
 {
-    cmd = add_command();
-    cmd->argv = malloc(sizeof(char *) * (cmd_size(t) + 1));
-    word_save(cmd, t);
-    while (t && t->type != TPIPE)
-    {
-        if(t && (t->type == T_REDIR_IN || t->type == T_REDIR_OUT ||t->type == T_REDIR_APPEND || t->type == T_HEREDOC))
-        {
-            if (t->next && t->next->type == TWORD)
-                redir_pars(cmd,t);
-            else
-                return NULL;
-        }
-        t = t->next;
-    }
-    if(t && t->type == TPIPE)
-    {
-        t = t->next;
-        if(!t)
-        {
-            return NULL;//buralar şimdilik böyle free falan yapılacak
-        }
-        cmd->i = 0;
-        cmd->next = parser(t,NULL);
-    }
-    return cmd;
+	cmd = add_command();
+	cmd->argv = malloc(sizeof(char *) * (cmd_size(t) + 1));
+	word_save(cmd, t);
+	while (t && t->type != TPIPE)
+	{
+		if (t && (t->type == T_REDIR_IN || t->type == T_REDIR_OUT
+				||t->type == T_REDIR_APPEND || t->type == T_HEREDOC))
+		{
+			if (t->next && t->next->type == TWORD)
+				redir_pars(cmd,t);
+			else
+				return (NULL);
+		}
+		t = t->next;
+	}
+	if (t && t->type == TPIPE)
+	{
+		t = t->next;
+		if (!t)
+			return (NULL);
+		cmd->i = 0;
+		cmd->next = parser(t, NULL);
+	}
+	return (cmd);
 }
--- redirect.c
+++ redirect.c
@@ -1,70 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakalkan <hakalkan@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/05 15:10:10 by hakalkan          #+#    #+#             */
+/*   Updated: 2026/05/05 15:10:10 by hakalkan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int exec_redir(t_command *cmd)
+int	exec_redir_in(char *filename)
 {
-    t_redir *tmp;
-    int     fd;
-    char    *filename;
+	int	fd;
 
-    tmp = cmd->redirs;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(filename);
+		free(filename);
+		return (-1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
 
-    if (cmd->heredoc_fd != -1)
-    {
-        dup2(cmd->heredoc_fd, STDIN_FILENO);
-        close(cmd->heredoc_fd);
-        cmd->heredoc_fd = -1;
-    }
+int	exec_redir_out(char *filename)
+{
+	int	fd;
 
-    while (tmp)
-    {
-        // Heredoc'u burada handle etme (exec_heredoc yapacak)
-        if (tmp->type == T_HEREDOC)
-        {
-            tmp = tmp->next;
-            continue;
-        }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+	{
+		perror(filename);
+		free(filename);
+		return (-1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
 
-        filename = for_quotes(tmp->file); // Quote'ları temizle
-        
-        if(tmp->type == T_REDIR_IN)
-        {
-            fd = open(filename, O_RDONLY);
-            if(fd < 0)
-            {
-                perror(filename);
-                free(filename);
-                return (-1);
-            }
-            dup2(fd, STDIN_FILENO);
-            close(fd);
-        }
-        else if(tmp->type == T_REDIR_OUT)
-        {
-            fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if(fd < 0)
-            {
-                perror(filename);
-                free(filename);
-                return (-1);
-            }
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
-        }
-        else if(tmp->type == T_REDIR_APPEND)
-        {
-            fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-            if(fd < 0)
-            {
-                perror(filename);
-                free(filename);
-                return (-1);
-            }
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
-        }
-        
-        free(filename);
-        tmp = tmp->next;
-    }
-    return (0);
+int	exec_redir_append(char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd < 0)
+	{
+		perror(filename);
+		free(filename);
+		return (-1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
+void	fd_check(t_command *cmd)
+{
+	if (cmd->heredoc_fd != -1)
+	{
+		dup2(cmd->heredoc_fd, STDIN_FILENO);
+		close(cmd->heredoc_fd);
+		cmd->heredoc_fd = -1;
+	}
+}
+
+int	exec_redir(t_command *cmd)
+{
+	t_redir	*tmp;
+	char	*filename;
+
+	tmp = cmd->redirs;
+	fd_check(cmd);
+	while (tmp)
+	{
+		// Heredoc'u burada handle etme (exec_heredoc yapacak)
+		if (tmp->type == T_HEREDOC)
+		{
+			tmp = tmp->next;
+			continue ;
+		}
+		filename = for_quotes(tmp->file); // Quote'ları temizle
+		if (tmp->type == T_REDIR_IN)
+			exec_redir_in(filename);
+		else if (tmp->type == T_REDIR_OUT)
+			exec_redir_out(filename);
+		else if (tmp->type == T_REDIR_APPEND)
+			exec_redir_append(filename);
+		free(filename);
+		tmp = tmp->next;
+	}
+	return (0);
 }
